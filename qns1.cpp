4ab257b077bf69fbdd8699a04bf0ac06fcf7a2bc
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-01-21 19:56:18 +0500

    Added qns1.cpp

---
+++ qns1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int num;
+cout<<"Input any number"; cin>>num;
+if(num>0)
+{
+    cout<< "number is posistive";
+}
+else if (num==0)
+{
+    cout << "Number is zero";
+
+}
+else
+{
+    cout <<"Number is Negative";
+}
+return 0;
+}
