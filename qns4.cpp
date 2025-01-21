768ba1da41f4d56bf85d0683e393bc8455d78aee
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-01-22 00:56:12 +0500

    update

--- qns4.cpp
+++ qns4.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 using namespace std;
-int main (){
-int 
+int main(){
+cout <<"Enter  Any year : ";
+int year;
+cin>>year;
+if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+{
+    cout <<"Its a leap year :";
 }
+else{
+    cout<<"Not a leap year: ";
+}
+return 0;
+}
+
+
+
+
+
+
+
+
+
