2367ddf19ab370d79d0f953f0cf1ca7d8bbd4f59
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-01-21 21:24:36 +0500

    updated

--- qns2.cpp
+++ qns2.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
 int main() {
-    
+    cout <<"Welcome to Grading system"<<endl;
+    int marks;
+    cout<<"Enter your marks: ";
+    cin>>marks;
+    if(marks>=90)
+    {
+        cout<<"Grade A";
+    }
+    else if(marks>=80)
+    {
+        cout<<"Grade B";
+    }
+    else if(marks>=70)
+    {
+        cout<<"Grade C";
+    }
+    else if(marks>=60)
+    {
+        cout<<"Grade D";
+    }
+    else if(marks>=50)
+    {
+        cout<<"Grade E";
+    }
+    else
+    {
+        cout<<"Grade F";
+    }
     return 0;
 }
