858c130ecb780e77fc04085af0d05a2246012842
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-01-23 00:55:53 +0500

    updated

--- qns10.cpp
+++ qns10.cpp
@@ -1,23 +1,28 @@
 //10.Create a program that checks if a given string is a palindrome.
+//10. Create a program that checks if a given string is a palindrome.
 #include <iostream>
 using namespace std;
-int main(){
-    string name,other_name;
-    cout <<"Enter Your First Name: "<<endl;
-    cin>>name;
-    cout <<"Enter Your sec-name to compare to find palindrome: "<<endl;
-    cin>>other_name;
-    bool is=false;
-    if(name == other_name)
-{
-    is==true;
-    cout <<"Name is palindrome:"<<endl;
-}
-else{
-    cout <<" Is not palindrome: "<<endl;
-}
 
-return 0;
+int main() {
+    string name;
+    cout << "Enter a string to check if it is a palindrome: " << endl;
+    cin >> name;
 
+    bool is_palindrome = true;
+    int length = name.length();
 
+    for (int i = 0; i < length / 2; ++i) {
+        if (name[i] != name[length - 1 - i]) {
+            is_palindrome = false;
+            break;
+        }
+    }
+
+    if (is_palindrome) {
+        cout << "The string is a palindrome." << endl;
+    } else {
+        cout << "The string is not a palindrome." << endl;
+    }
+
+    return 0;
 }
