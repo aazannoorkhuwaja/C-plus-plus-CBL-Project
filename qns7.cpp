d1505bcbd650fd3c360139de3ed853868e436286
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-01-22 13:55:25 +0500

    update

--- qns7.cpp
+++ qns7.cpp
@@ -2,5 +2,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+    cout <<"Input the three sides of triangle : "<<endl;
+    int s1,s2,s3;
+    cin>>s1>>s2>>s3;
+   if (s1 > 0 && s2 > 0 && s3 > 0 && s1 + s2 > s3 && s2 + s3 > s1 && s3 + s1 > s2)
+    {
+        cout <<"Triangle is Valid "<<endl;
+    }
+    else{
+         cout <<"Triangle is not Valid "<<endl;
+    }
+return 0;
 }
--- qns8.cpp
+++ qns8.cpp
@@ -1,1 +1,24 @@
 //8. Create a program that converts a given temperature from Celsius to Fahrenheit.
+#include <iostream>
+using namespace std;
+int main (){
+    cout <<"Give Character Temperature Will Convert that into Fahrenhiet and Kelvin! (f/F and k/K) :"<<endl;
+    int temp;
+    char ch ;
+    cin>>ch;
+    cout<<"Enter You Temperature In Celcius: "<<endl; 
+    cin>>temp;
+    if (ch == 'f' || ch == 'F'){
+        temp=(1.8*temp)+32;
+        cout<<"Temperature in Fahrenheit is: "<<temp<<endl; 
+    }
+    else if(ch == 'k' || ch == 'k')
+    {
+        temp = 273 + temp;
+    cout<<"Temperature in Kelvin is: "<<temp<<endl; 
+    }
+    else{
+        cout<<"Not Valid Try again!"<<endl; 
+    }
+
+}
