1fbc0ef1c1529d9f4bc74f19391a76a09b50bdb2
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-01-27 19:33:59 +0500

    updated

--- IF-Else/qns22.cpp
+++ IF-Else/qns22.cpp
@@ -1,1 +1,31 @@
 //22.Write a program that gives a feedback score based on user rating (1-5).
+#include <iostream>
+using namespace std;
+int main()
+{
+    int score;
+    cout<<"Enter your score from 1-5"<<endl;
+    cin>>score;
+    if(score>0 && score <6)
+    {
+        if (score==1){
+        cout <<"Not Good!"<<endl;
+        }
+        else if (score == 2){
+            cout <<"good! "<<endl;
+        }
+        else if(score == 3){
+            cout<<"Better!"<<endl;
+        }
+         else if(score == 4){
+            cout<<"Best!"<<endl;
+        }
+         else if(score == 5){
+            cout<<"Excellent!"<<endl;
+        }
+    }
+    else{
+        cout <<"Score not valid score can be from 1 - 5"<<endl;
+    }
+    return 0;
+}
