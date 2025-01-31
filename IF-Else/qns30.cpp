bce4e58933e201857d12a044e8798cc9a381fd17
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-01-31 19:23:05 +0500

    updated

--- IF-Else/qns30.cpp
+++ IF-Else/qns30.cpp
@@ -1,1 +1,21 @@
 //30.Write a program to determine the color of a traffic light based on its input.
+#include <iostream>
+using namespace std;
+
+int main() {
+    char light;
+    cout << "Enter the traffic light color (R for Red, Y for Yellow, G for Green): ";
+    cin >> light;
+
+    if (light == 'R' || light == 'r') {
+        cout << "Stop" << endl;
+    } else if (light == 'Y' || light == 'y') {
+        cout << "Ready to move" << endl;
+    } else if (light == 'G' || light == 'g') {
+        cout << "Go" << endl;
+    } else {
+        cout << "Invalid input" << endl;
+    }
+
+    return 0;
+}
