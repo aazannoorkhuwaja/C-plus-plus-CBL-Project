728cceba45837dc362f9d06de818a80dc706a7b3
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-01-31 23:04:50 +0500

    updated

--- Loop/qns2.cpp
+++ Loop/qns2.cpp
@@ -1,11 +1,22 @@
 //2. Create a program that prints the sum of the first n natural numbers.
 #include<iostream>
 using namespace std;
-int main(){
-
-
-
-
-
-
+int main ()
+{
+    int limit,num=0,sec=0,sum=0;
+    cout <<"Enter Your Limit upto which you want to add numbers ! "<<endl;
+    cin>>limit;
+    if(limit>0){
+        while (num<limit)
+        {
+            num++;
+            sec=num;
+            sum=sum+sec;
+        }
+        cout <<"Sum Of numbers upto (limit) is :"<<sum<<endl;
+    }
+    else{
+        cout <<"Limit Is  Not Valid try again !"<<endl;
+    }
+    return 0;
 }
---
+++ Loop/qns3.cpp
@@ -0,0 +1,1 @@
+//3. Write a program to print the factorial of a number using a loop.
