a2c3a376f6615bd89c930fdb3590d389f2ec6ae8
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-01-31 19:15:14 +0500

    updated

--- IF-Else/qns29.cpp
+++ IF-Else/qns29.cpp
@@ -1,1 +1,16 @@
 //29.Create a program that checks if a person is eligible to vote based on age.
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout <<"enter your age"<<endl;
+    cin>>age;
+    if (age>0 && age<18){
+         cout <<"Not valid to vote!"<<endl;
+        
+    }
+    else {
+        cout<<"Valid to vote !"<<endl;
+    }
+    return 0;
+}
