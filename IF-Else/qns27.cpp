cb811f7dcf349bba54273b547aeca171b1cfafff
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-01-31 13:07:11 +0500

    updated

--- IF-Else/qns27.cpp
+++ IF-Else/qns27.cpp
@@ -1,1 +1,17 @@
 //27.Create a program that checks if a number is a Fibonacci number.
+#include<iostream>
+using namespace std;
+int main (){
+    int num;
+    cout <<"Enter any number :"<<endl;
+    cin>>num;
+    if(num%1==0 || num%1==0 || num%2==0 || num%3==0 || num%5==0 || num%8==0 || num%13==0)
+    {
+        cout <<"Number is from fibonacci:"<<endl;
+
+    }
+    else{
+        cout<<"Number is not from fibonacci :"<<endl;
+    }
+    return 0;
+}
--- IF-Else/qns28.cpp
+++ IF-Else/qns28.cpp
@@ -1,1 +1,18 @@
 //28.Write a program that checks whether a character is a special character.
+#include<iostream>
+using namespace std;
+int main ()
+{
+char ch;
+cout <<"enter any specail character! :"<<endl;
+cin>>ch;
+if (ch>32 && ch<48 || ch > 57 &&  ch<65 || ch >90 && ch<97 || ch >122 && ch <127 ){
+   cout <<"Character is special"<<endl;
+}
+else {
+    cout <<"Character is not special"<<endl;
+}
+
+return 0;
+
+}
