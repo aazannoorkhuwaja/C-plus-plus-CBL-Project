eed21a554d79f8640d297b36f47f2119f3ea13b4
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-02-01 17:23:31 +0500

    updated

--- Loop/qns4.cpp
+++ Loop/qns4.cpp
@@ -3,11 +3,15 @@
 using namespace std;
 int main ()
 {
-    int n,first,sec,sum=0;
+    int n,first=0,sec=1,sum;
     cout <<"Enter any number: "<<endl;
     cin>>n;
-    for(int i=0;i<n;i++){
-        
-
+    cout<<first<<" "<<sec<<" ";
+    for(int i=2;i<n;i++){
+        sum=first+sec; // here we get 1,2,3,5,8....
+        cout <<sum<<" ";
+        first=sec; // this will transfer 1 to first
+        sec=sum; //and this will trasnfer add of first two which is 1 to sec
     }
+    return 0;
 }
--- Loop/qns5.cpp
+++ Loop/qns5.cpp
@@ -1,1 +1,6 @@
 //5. Write a program to display all prime numbers between 1 and 100.
+#include<iostream>
+using namespace std;
+int main(){
+    
+}
