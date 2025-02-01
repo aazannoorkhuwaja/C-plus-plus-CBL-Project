034d2d3d7990fb5e3cbfead73036c52ed04dbd38
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-02-01 22:49:16 +0500

    updated

--- Loop/qns5.cpp
+++ Loop/qns5.cpp
@@ -2,5 +2,21 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int count;
+    for(int i=1;i<=100;i++)
+    {
+        count=0;
+        for(int j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                count++;
+            }
+        }
+            if(count==2)
+            {
+                cout<<i<<" ";
+            }
+    }
+    return 0;
 }
