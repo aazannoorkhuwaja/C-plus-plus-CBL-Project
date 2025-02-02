42c965b79cfc27cd5a4dbe8b26fd5f9338483f1b
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-02-02 21:55:52 +0500

    updated

--- Loop/qns15.cpp
+++ Loop/qns15.cpp
@@ -1,1 +1,32 @@
 //15.Write a program to find the GCD of two numbers using loops.
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n1,n2,gcd,count1=0,count2=0,save1,save2;
+    cout<<"Enter two numbers :";cin>>n1>>n2;
+    if(n1<n2){
+    for(int i=1;i<=n1;i++)
+    {
+        if(n1%i==0 && n2%i==0)
+        {   
+            save1=i;
+        }
+
+    }
+    }
+    else{
+        for(int i=1;i<=n2;i++)
+    {
+        if(n1%i==0 && n2%i==0)
+        {   
+            save1=i;
+        }
+
+    }
+    } 
+
+    cout <<"The Greatest GCD is :"<<save1<<endl;
+    
+
+}
---
+++ Loop/qns16.cpp
@@ -0,0 +1,3 @@
+//16.Create a program to print a triangle of stars.
+#include <iostream>
+using namespace std; 
