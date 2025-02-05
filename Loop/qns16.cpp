b18b8364f1011ffa3590040ea7ac738d8804c5d5
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-02-05 16:12:43 +0500

    updated

--- Loop/qns16.cpp
+++ Loop/qns16.cpp
@@ -1,3 +1,12 @@
 //16.Create a program to print a triangle of stars.
 #include <iostream>
 using namespace std; 
+for(int i=0;i<n;i++)
+{
+    for(int i=n;i>0;i--)
+    {
+        cout<<"*";
+    }
+
+    cout <<endl;
+}
