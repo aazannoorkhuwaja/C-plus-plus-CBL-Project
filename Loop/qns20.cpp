09dabca3e0cead4e8b2f00f61043cb96158c3aa8
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-02-05 18:46:14 +0500

    updated

--- Loop/qns20.cpp
+++ Loop/qns20.cpp
@@ -1,7 +1,14 @@
-//
+//20.Create a program that prints a square of stars.
 #include <iostream>
 using namespace std;
 int main()
 {
-    
+for(int i=0;i<4;i++)
+{
+    for(int j=0;j<4;j++)
+    {
+        cout <<" *";
+    }
+    cout <<endl;
+}
 }
