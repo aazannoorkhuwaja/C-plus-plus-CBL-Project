211a33b6729ba6b77d2fbb8f8956f3d9d51bc733
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-02-05 16:46:25 +0500

    updated

---
+++ Loop/qns19.cpp
@@ -0,0 +1,10 @@
+//19.Write a program that prints the first 10 square numbers.
+#include<iostream>
+using namespace std;
+int main ()
+{
+    for(int i=1;i<=10;i++)
+    {
+        cout <<"Square of num is:"<<i*i<<endl;
+    }    
+}
