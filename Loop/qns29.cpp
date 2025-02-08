132736e8f4eb04ffcc052d25fb2aafac6712c13e
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-02-09 03:05:18 +0500

    updated

--- Loop/qns29.cpp
+++ Loop/qns29.cpp
@@ -1,2 +1,14 @@
 //29.Write a program that generates random numbers and prints them until a certain number is reached.
-
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cout <<"enter number upto which you want random number:"<<endl;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Rand number is: "<<rand()%n<<endl;
+    }
+return 0;
+}
--- Loop/qns30.cpp
+++ Loop/qns30.cpp
@@ -1,2 +1,41 @@
 //30.Create a program to find the maximum difference between any two elements in a series of inputs.
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n, inp;
+    
+    cout << "Enter number of elements in the series: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Invalid input! Series must have at least one number." << endl;
+        return 0;  
+    }
+
+    
+    cout << "Enter number: ";
+    cin >> inp;
+    
+    int max_value = inp;  
+    int min_value = inp;  
+
+    
+    for (int i = 1; i < n; i++) {
+        cout << "Enter number: ";
+        cin >> inp;
+
+        if (inp > max_value) {
+            max_value = inp;
+        }
+
+        if (inp < min_value) {
+            min_value = inp;
+        }
+    }
+
+    int max_difference = max_value - min_value;
+    cout << "The maximum difference is: " << max_difference << endl;
+
+    return 0;
+}
