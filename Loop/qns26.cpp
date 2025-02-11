892cf47ce8d65bb6c21112e2999106e8ad793a87
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-02-11 18:34:51 +0500

    updated

--- Loop/qns26.cpp
+++ Loop/qns26.cpp
@@ -4,12 +4,14 @@ using namespace std;
 int main()
 {
     for(int i=1;i<=3;i++)
-    {
+      {
          for(int j=1;j<=i;j++)
          {
-            cout<<" "; 
+            cout << " "; 
          }
-          cout<<"*"<<endl;
-    }
+            cout<<"*"<<endl;
+      }
+
+
     return 0;
 }
