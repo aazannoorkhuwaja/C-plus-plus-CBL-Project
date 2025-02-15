2dfbdf5c64c01e8ff888ae7dd26ed88c52333fcd
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-02-16 00:01:13 +0500

    updated

--- Arrays/qns4.cpp
+++ Arrays/qns4.cpp
@@ -1,15 +1,22 @@
 //4. Create a program to reverse the elements of an array. 
 #include<iostream>
 using namespace std;
-int main (){
-    int arr[]={1,5,2,13,3},size;
+int main ()
+{
+    int arr[]={1,5,2,13,3},size,sav,first;
     size = sizeof(arr)/sizeof(int);
-        for(int j=0;j<size/2;j--)
+        for(int j=0;j<size/2;j++)
         {
+           
+            sav=arr[size-(j+1)];
+            first=arr[j];
+            arr[j]=sav;
+            arr[size-(j+1)]=first;
             
-
-
-
+        }
+        for(int j=0;j<size;j++)
+        {
+           cout <<"The reverse array is :"<<arr[j]<<endl;
         }
 
-    }
+}
---
+++ Arrays/qns5.cpp
@@ -0,0 +1,6 @@
+//#5. Write a program to sort an array in ascending order using bubble sort. 
+#include<iostream>
+using namespace std;
+int main (){
+    int 
+}
