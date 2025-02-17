a66e3d7362372f9a07a0ea264bc8659c66dad3ed
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-02-18 00:57:08 +0500

    updated

--- Arrays/qns8.cpp
+++ Arrays/qns8.cpp
@@ -4,20 +4,23 @@ using namespace std;
 int main ()
 {
     int size=5,arr[size]={1,1,2,3,2},count=0;
-    for (int i=0;i<size;i++)
+   
+    for(int i=0;i<size;i++)
     {
-       for (int j=0;j<size;j++)
+        for(int j=0;j<size;j++)
+        {
+            if(arr[i]==arr[j])
             {
-                if(i!=j){
-                if(arr[i]==arr[j])
-                {
-                    count++;
-                }
-                    }
-                
+                count++;
             }
+        }
+        if(count>0)
+        {
+            cout <<"The Number: "<<arr[i]<<" Occured :"<<count<<endl;
+            count=0;
+        }
     }
 
-        cout <<"The Number Occured: "<<count <<endl;
+       
     return 0;
 }
